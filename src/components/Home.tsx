2857541e4ead4ce1468876a7c57940abaa0ef10f
JuanMaCarrearas <manuucarreras@gmail.com>
2024-06-27 18:28:06 -0300

    NavBar component

---
+++ src/components/Home.tsx
@@ -0,0 +1,19 @@
+import NavBar from "./NavBar"
+
+function Home() {
+
+
+
+
+  return (
+    <>
+      <NavBar />
+      <section>
+        <h1>Hola soy el Home</h1>
+      </section>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <>
+        <nav>
+            <ul>
+              <li>Inicio</li>
+              <li>Precios</li>
+              <li>Preguntas Frecuentes</li>
+              <li>Descargar App</li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
