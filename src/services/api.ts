2863676f661f5791b54523579890d3d0254cfd2b
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-01 17:44:36 -0300

    add api params

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,19 @@
 import axios from 'axios'
+import DataConi from '../types/getData.type'
 
-const BASE_URL = ''
-const API_KEY = ''
+const BASE_URL = 'https://api.coingecko.com/api/v3'
+const API_KEY = 'CG-u4hop6WZj65C4VD5HFfwqbJW'
 
 
 const api = axios.create({
-  
+  baseURL: BASE_URL,
+  headers: {
+    'Content-type': 'application/json',
+    'x-cg-demo-api-key' : API_KEY
+  }
 })
+
+export const getData = async () => {
+    const response = await api.get<DataConi[]>('/coins/list')
+    return response.data
+}
