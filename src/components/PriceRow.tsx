b65856f1462b6d8706bd4dce85441f6e49caff86
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-11 19:22:07 -0300

    Added styles to component

--- src/components/PriceRow.tsx
+++ src/components/PriceRow.tsx
@@ -7,27 +7,27 @@ interface PriceProp {
 function PriceRow({coin}: PriceProp) {
   return (
     <>
-      <tbody>
-        <tr>
-          <td>
-            <p className='mx-5'>{coin.market_cap_rank}</p>
+      <tr className=' border-b-2 border-green-700 '>
+        <td  className='w-20 p-3 text-sm text-center '>
+          <p className='mx-2 font-medium'>{coin.market_cap_rank}</p>
+        </td>
 
-            <img 
-              src={coin.image} 
-              alt={coin.name} 
-              className='mx-3 w-10 h-10'
-            />
+        <td  className='flex w-72 p-3 text-sm text-left items-center '>
+          <img 
+            src={coin.image} 
+            alt={coin.name} 
+            className='mr-4 w-7 h-7'
+          />
+          <div className='[&>span]:block'>
+            <span className='text-[1rem] font-semibold'>{coin.name}</span>
+            <span className='text-xs uppercase text-gray-500 font-medium'>{coin.symbol}</span>
+          </div>
+        </td>
 
-            <span className='block text-xl'>{coin.name}</span>
-            <span className='block'>{coin.symbol}</span>
-          </td>
-
-          <td>${coin.current_price.toLocaleString()}</td>
-
-          <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-          <td>${coin.market_cap.toLocaleString()}</td>
-        </tr>  
-      </tbody>
+        <td  className='w-32 p-3 text-sm text-right font-medium'>${coin.current_price.toLocaleString()}</td>
+        <td  className='w-32 p-3 text-sm text-right font-medium'>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+        <td  className='w-52 p-3 text-sm text-right font-medium'>${coin.market_cap.toLocaleString()}</td>
+      </tr>  
     </>
   )
 }
