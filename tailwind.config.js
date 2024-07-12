62a9b603660b1e0c966ebc30d9a04afa98b95515
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-12 20:28:02 -0300

    Added page colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //cambiar ref a ingles!!
+
+        azul: '#011640',
+        verdeSemiClaro: '#03402F',
+        verdeHover: '#021710',
+        verdeClaro1: '#07F2B0',
+        verdeClaro: '#0A7355',
+        verdeSemiOscuro: '#07261D',
+        verdeOscuro: '#010907',
+        verdeOscuro2: '#020D09',
+
+        // tablas 
+        grisOscuro: '#0B0C0D',
+        grisClaro: '#0F1110',
+        grisLetras: '#868C8A',
+        gris1: '#1F2523'
+      }
+    },
   },
   plugins: [],
 }
