54976e15394f661acaa7f61801e6867164642014
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-13 16:09:19 -0300

    Added styles to component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,25 @@
+
+interface SerchBarProps {
+  search: string
+  setSearch: (value: string) => void
+}
+
+
+function SearchBar({search, setSearch}: SerchBarProps) {
+
+  return (
+    <>
+      <div className='flex justify-end mr-20'>
+        <input 
+          type='text' 
+          placeholder='Buscar...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value) }
+          className='outline-none w-80 h-7 pl-4 py-4 rounded-2xl border-2 border-gris1 bg-transparent placeholder:text-grisLetras focus:border-verdeClaro focus:bg-verdeHover transition duration-500'
+        />
+      </div>
+    </>
+  )
+}
+
+export default SearchBar
