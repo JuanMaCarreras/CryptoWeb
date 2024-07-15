cde0270419da0d4302da699bd9dcfb3afacd7cc5
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-15 19:03:33 -0300

    NavBar structured

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+
+function NavBar() {
+  return (
+    <>
+      <nav className=' w-full h-20 bg-gray-900'>
+        <ul className='flex text-grisLetras justify-around border-2 border-red-600  '>
+          <li>Inicio</li>
+          <li>Precios</li>
+          <li>Aprender Más</li>
+        </ul>
+
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
