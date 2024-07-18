9645905ebe193fa9ffea89a5df01c70b11a7ed06
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-18 19:39:42 -0300

    Added Intro component

---
+++ src/components/Intro.tsx
@@ -0,0 +1,15 @@
+
+function Intro() {
+  return (
+    <>
+      <section className='h-screen flex flex-col items-center justify-center'>
+        <h2 className='text-3xl'
+          >Descubre el Mundo Cripto con Lynx
+        </h2>
+        <h3>Toda la Información que Necesitas</h3>
+      </section>
+    </>
+  )
+}
+
+export default Intro
