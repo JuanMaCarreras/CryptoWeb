1c5c8f4e56abc8043ef8629fd700ecb00e7a8082
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-22 20:21:17 -0300

    Added Description page

--- src/page/Description.tsx
+++ src/page/Description.tsx
@@ -7,10 +7,23 @@ function Description() {
           <h1 className='ml-16 text-2xl'>Proyecto Tecnico</h1>
 
           <div className='mx-20 mt-14'>
-            <p className='text-center'>Esta página web es un proyecto personal, creado con la intención poner a pruba conocimientos de programación 
+            <p className='text-center text-grisLetras'>Esta página web es un proyecto personal, creado con la intención poner a pruba conocimientos de programación 
             y a su vez ser util para usuarios que buscar informance sobre el mundo de las cripto monedas de una manera simple y sencilla.
             </p>
-            <h4 className='mt-14 text-xl'>Tecnologías usadas:</h4>
+            <h4 className='mt-14  text-xl'>Tecnologías usadas:</h4>
+            <p className='text-center text-grisLetras mt-11 mb-20'>Usando Vite como empaquetador, React para la interface de usuario y TailwindCSS para los estilos. TypeScript para un mejor 
+              manejo de los datos al hacer peticiones con Axios a la API de Coin Gecko, Recharts para los gráficos de las criptos. Y Wouter
+              para una rápida y sencilla forma de enrutador. Además, el uso del logo obtenido de Vecteezy 
+            </p>
+            <ul className=' flex ml-4 justify-evenly'>
+              <li>Vite</li>
+              <li>React</li>
+              <li>TailwindCSS</li>
+              <li>TypeScript</li>
+              <li>Axios</li>  
+              <li>Wouter</li>
+              <li>Recharts</li>  
+            </ul>
           </div>
           
         </section> 
