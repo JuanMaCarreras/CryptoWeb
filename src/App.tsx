5e1650d628b65c6b8d50ba38a43e82d260cc604f
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-22 19:42:27 -0300

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-// import Cryptocurrency from './page/Cryptocurrency'
 import NavBar from './components/NavBar'
+import Home from './page/Home'
+import Cryptocurrency from './page/Cryptocurrency'
+import Description from './page/Description'
+import { Route, Switch } from 'wouter'
 import './App.css'
 
 function App() {
   return (
     <>
       <NavBar />
-      {/* <Cryptocurrency /> */}
+      <Switch>
+        <Route path='/' component={Home}  />
+        <Route path='price' component={Cryptocurrency} />
+        <Route path='description' component={Description} />
+      </Switch>
     </>
   )
 }
