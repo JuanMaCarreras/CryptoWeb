7aa99163d615f752dc15f23405d76e808b47dda2
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-22 18:42:13 -0300

    Added Description component

---
+++ src/page/Description.tsx
@@ -0,0 +1,22 @@
+
+function Description() {
+  return (
+    <>
+      <article className='w-full'>
+        <section className=' text-white pt-32'>
+          <h1 className='ml-16 text-2xl'>Proyecto Tecnico</h1>
+
+          <div className='mx-20 mt-14'>
+            <p className='text-center'>Esta página web es un proyecto personal, creado con la intención poner a pruba conocimientos de programación 
+            y a su vez ser util para usuarios que buscar informance sobre el mundo de las cripto monedas de una manera simple y sencilla.
+            </p>
+            <h4 className='mt-14 text-xl'>Tecnologías usadas:</h4>
+          </div>
+          
+        </section> 
+      </article>
+    </>
+  )
+}
+
+export default Description
