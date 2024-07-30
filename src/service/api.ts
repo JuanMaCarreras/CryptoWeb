345035cf83ed1c3872c87f02ad99e7c3246a84aa
JuanMaCarrearas <manuucarreras@gmail.com>
2024-07-30 19:37:37 -0300

    Fixed chart functionality and API call

--- src/service/api.ts
+++ src/service/api.ts
@@ -25,11 +25,11 @@ export const getCoinMarket = async (perPage = 50): Promise<MarketCoins[]> => {
   return response.data
 }
 
-export const getCoinChartMarket = async (coinId: string, days: number): Promise<CoinMarketChart> => {
+export const getCoinChartMarket = async (coinId: string): Promise<CoinMarketChart> => {
   const response = await api.get<CoinMarketChart>(`/coins/${coinId}/market_chart`, {
     params: {
       vs_currency: 'usd',
-      days,
+      days: 30
     }
   })
   return response.data
