33fa93fa07d2b6c82c2a89a8f524fd1446daa0f3
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-01 19:35:06 -0300

    complete functionality for CoinDetails component

--- src/components/CoinDetails.tsx
+++ src/components/CoinDetails.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'wouter'
+import { CoinDetailsById  } from '../types/Coins'
+import { getCoinById } from '../service/api'
+import Chart from './Chart'
 
 function CoinDetails() {
+  const { coinId } = useParams<{coinId: string}>()
+  const [coins, setCoins] = useState<CoinDetailsById>()
+
+  useEffect(() =>{
+    const fetchDetailById = async () => {
+      try {
+        const res = await getCoinById(coinId)
+        console.log(res)
+        setCoins(res)
+
+      } catch (error) {
+        console.log('fetch details data', error)
+      }
+    }
+    fetchDetailById()
+
+  },[coinId])
+
   return (
     <>
+      <section className='text-white'>
+        <h2>{coins?.name} {coins?.symbol}</h2>
+        <Chart coinId={coinId}/>
+      </section>
     </>
   )
 }
