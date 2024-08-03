3c6783c65be94705c59b7b6a9c01fb7e1fb00905
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-03 16:31:00 -0300

    Add chart components from shadcn/ui

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react'
 import { getCoinChartMarket } from '../service/api'
 import { CoinMarketChart } from '../types/Coins'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
 
 interface DataParams {
   coinId: string
@@ -11,6 +17,13 @@ interface DataChart {
   date: string
   price: number
 }
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig
 
 function Chart ({coinId}: DataParams) {
 
@@ -42,15 +55,22 @@ function Chart ({coinId}: DataParams) {
 
   return (
     <>
-     <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={dataChart}>
-        <CartesianGrid strokeDasharray="2 2" />
-        <YAxis domain={[minPrice * 0.95, maxPrice * 1.05]} />
-        <XAxis dataKey='date' />
-        <Tooltip />
-        <Line type="monotone" dataKey="price" stroke="#07F2B0" dot={false} />
-      </LineChart>
-     </ResponsiveContainer>
+      <ChartContainer config={chartConfig}>
+       <ResponsiveContainer width="100%" height={400}>
+        <LineChart accessibilityLayer data={dataChart} margin={{left: 12, right: 12,}}>
+          <CartesianGrid strokeDasharray="2" vertical={false} />
+          <YAxis domain={[minPrice * 0.95, maxPrice * 1.05]} />
+          <XAxis dataKey='date '  
+            tickLine={false}
+            axisLine={false}
+            tickMargin={8}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />}/>
+          <Line type="linear" dataKey="price" stroke="#07F2B0" dot={false} strokeWidth={2}/>
+        </LineChart>
+       </ResponsiveContainer>
+      </ChartContainer>
     </>
   )
 }
