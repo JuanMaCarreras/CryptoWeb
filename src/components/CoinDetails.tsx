e2a7bcb1539ffd16978399d400759649bd8e5a52
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-07 18:48:22 -0300

    Added select currency to CoinDetails component

--- src/components/CoinDetails.tsx
+++ src/components/CoinDetails.tsx
@@ -13,14 +13,16 @@ import {
 } from '@/components/ui/card'
 
 interface Prop {
-  currency: string
+  currency: string;
 }
 
 
-function CoinDetails({currency}:Prop) {
+function CoinDetails({currency}: Prop) {
 
   const { coinId } = useParams<{coinId: string}>()
-  const [coins, setCoins] = useState<CoinDetailsById>()
+  const [coins, setCoins] = useState<CoinDetailsById | null>( null)
+
+  console.log(currency)
 
   useEffect(() =>{
     const fetchDetailById = async () => {
@@ -39,31 +41,43 @@ function CoinDetails({currency}:Prop) {
   
   return (
     <>
-      <Card className='text-white bg-verdeOscuro'>
-        <CardHeader className='mb-4'>
-          <CardTitle className='flex items-center mb-5'>
+      <Card className='text-white bg-verdeOscuro my-8'>
+        <CardHeader className='mb-5'>
+          <CardTitle className='flex items-center mb-6'>
             <img src={coins?.image.small} alt={coins?.name} className='mr-3 w-9 h-9'/> 
             <h2 className='text-3xl'>{coins?.name}</h2> 
-            <span className='uppercase text-sm text-grisLetras ml-3'>{coins?.symbol}</span>
+            <span className='uppercase text-sm text-grisLetras ml-3 mt-2'>{coins?.symbol}</span>
           </CardTitle>
           <CardDescription className='flex justify-around'>
            <dl> 
             <dt className='text-grisLetras font-semibold'>Precio Actual</dt>
-            <dd className='font-medium text-xl'>${coins?.market_data.current_price.currency}</dd>
+            <dd className='font-medium text-xl'>
+              ${coins?.market_data.current_price[currency].toLocaleString()}
+              <span className='uppercase mx-1'>{currency}</span>
+            </dd>
            </dl>
 
            <dl> 
             <dt className='text-grisLetras font-semibold'>24H Máximo</dt>
-            <dd className='font-medium text-xl'>${coins?.market_data.high_24h.currency}</dd>
+            <dd className='font-medium text-xl'>
+              ${coins?.market_data.high_24h[currency].toLocaleString()}
+              <span className='uppercase mx-1'>{currency}</span>
+            </dd>
            </dl>
 
            <dl> 
             <dt className='text-grisLetras font-semibold'>24H Mínimo</dt>
-            <dd className='font-medium text-xl'>${coins?.market_data.low_24h.currency}</dd>
+            <dd className='font-medium text-xl'>
+              ${coins?.market_data.low_24h[currency].toLocaleString()}
+              <span className='uppercase mx-1'>{currency}</span>
+            </dd>
            </dl>
            <dl> 
             <dt className='text-grisLetras font-semibold'>Capitalización de Mercado</dt>
-            <dd className='font-medium text-xl'>${coins?.market_data.market_cap.currency}</dd>
+            <dd className='font-medium text-xl'>
+              ${coins?.market_data.market_cap[currency].toLocaleString()}
+              <span className='uppercase mx-1'>{currency}</span>
+            </dd>
            </dl>
           </CardDescription>
         </CardHeader>    
