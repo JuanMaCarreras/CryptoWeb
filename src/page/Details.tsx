30f8b6d0f114f8aaf1476be3d38035ada7be0b2b
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-07 18:46:49 -0300

    Added Details page

--- src/page/Details.tsx
+++ src/page/Details.tsx
@@ -1,16 +1,27 @@
-import { useState, useEffect } from 'react'
-import CoinDetails from '@/components/CoinDetails'
+import { useState } from 'react'
+// import CoinDetails from '@/components/CoinDetails'
+import CurrencySelect from '@/components/CurrencySelect';
 
 function Details() {
 
   const [currency, setCurrency] = useState<string>('usd')    
 
-  useEffect(() => {
-    setCurrency('usd')
-  },[])
+  // useEffect(() => {
+  //   setCurrency('usd')
+  // },[])
+
+  const handleCurrencyChange = (value: string) => {
+    setCurrency(value);
+  };
+
+  console.log(currency)
+
   return (
     <>
-      <CoinDetails currency={currency}/>
+      <header className='text-white'>
+        <CurrencySelect value={currency} onChange={handleCurrencyChange}/>
+      </header>
+      {/* <CoinDetails currency={currency}/> */}
     
     </>
   )
