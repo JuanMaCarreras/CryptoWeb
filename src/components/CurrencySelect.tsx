0ad53dba9082aeff869b8d590d49b406c04e8603
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-07 18:26:00 -0300

    Added Select Currency component

---
+++ src/components/CurrencySelect.tsx
@@ -0,0 +1,41 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+interface Props {
+  value: string
+  onChange: (value: string) => void
+}
+
+function CurrencySelect({value, onChange}:Props) {
+
+  const options = [
+    { value: 'usd', label: 'USD' },
+    { value: 'ars', label: 'ARS' },
+    { value: 'eur', label: 'EUR' },
+  ]
+
+
+  return (
+    <>
+      <Select value={value} onValueChange={onChange}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Divisas" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map(option => (
+            <SelectItem className='text-white' key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </>
+  )
+}
+
+export default CurrencySelect
