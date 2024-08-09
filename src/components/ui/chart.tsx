e15dd706ee7e00998c15ad7f16681d93348a7f25
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-09 12:58:30 -0300

    Change the color of shadcn/ui components

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -177,12 +177,12 @@ const ChartTooltipContent = React.forwardRef<
       <div
         ref={ref}
         className={cn(
-          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
+          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-darkGray px-2.5 py-1.5 text-xs shadow-xl",
           className
         )}
       >
         {!nestLabel ? tooltipLabel : null}
-        <div className="grid gap-1.5">
+        <div className="grid gap-5">
           {payload.map((item, index) => {
             const key = `${nameKey || item.name || item.dataKey || "value"}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
