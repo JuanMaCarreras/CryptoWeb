d13e1c5ac209cd896f7b48c6feece38c783627bf
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-09 17:31:35 -0300

    Add icon and link for details

--- src/components/CoinsRow.tsx
+++ src/components/CoinsRow.tsx
@@ -1,8 +1,10 @@
-import { MarketCoins } from '../types/Coins'
+import { MarketCoins } from '@/types/Coins'
+import { Link } from 'wouter'
 import {
   TableCell,
   TableRow,
-} from "@/components/ui/table"
+} from '@/components/ui/table'
+import { HiOutlineExternalLink } from 'react-icons/hi'
 
 interface CoinProp {
   coin: MarketCoins
@@ -12,31 +14,36 @@ interface CoinProp {
 
 function CoinsRow({coin, index}: CoinProp) {
 
-  const priceColorClass =  coin.price_change_percentage_24h >= 0 ? 'text-green-400' : 'text-red-400'
+  const priceColorClass =  coin.price_change_percentage_24h >= 0 ? 'text-positiveNum' : 'text-negativeNum'
   const tableColor = index % 2 === 0 ? 'bg-deepGray' : 'bg-darkGray'
 
   return (
     <>
-      <TableRow className={`text-sm [&_td]:p-4 ${tableColor}`}>
-        <TableCell className="text-center" >
-          {coin.market_cap_rank}
-        </TableCell>
+      <TableRow className={`text-sm ${tableColor}`}>
+          <TableCell className='text-center' >
+             {coin.market_cap_rank}
+          </TableCell>
 
-        <TableCell className='flex'>
-          <img 
-            src={coin.image} 
-            alt={coin.name} 
-            className='mr-4 w-7 h-7'
-          />
-          <div className='[&>span]:block'>
-            <span className='text-[1rem] font-medium'>{coin.name}</span>
-            <span className='text-xs uppercase text-textGray font-medium'>{coin.symbol}</span>
-          </div>
-        </TableCell>
+          <TableCell className='flex'>
+            <img 
+              src={coin.image} 
+              alt={coin.name} 
+              className='mr-4 w-7 h-7 mt-1'
+            />
+            <div className='[&>span]:block'>
+              <span className='text-[1rem] font-medium'>{coin.name}</span>
+              <span className='text-xs uppercase text-textGray font-medium'>{coin.symbol}</span>
+            </div>
+          </TableCell>
 
-        <TableCell className="text-right">${coin.current_price.toLocaleString()}</TableCell>
-        <TableCell className={`text-right ${priceColorClass}`}>{coin.price_change_percentage_24h.toFixed(2)}%</TableCell>
-        <TableCell className="text-center">${coin.market_cap.toLocaleString()}</TableCell>
+          <TableCell className='text-right'>${coin.current_price.toLocaleString()}</TableCell>
+          <TableCell className={`text-right ${priceColorClass}`}>{coin.price_change_percentage_24h.toFixed(2)}%</TableCell>
+          <TableCell className='text-center'>${coin.market_cap.toLocaleString()}</TableCell>
+          <TableCell>
+            <Link to={`/coin/${coin.id}`} >
+              <HiOutlineExternalLink className='h-5 w-5 text-textGray hover:text-brightGreen transition duration-500'/>
+            </Link>
+          </TableCell>
       </TableRow>  
     </>
   )
