c69a05a111c64c2809f5dfc5680f7aff7d683966
JuanMaCarrearas <manuucarreras@gmail.com>
2024-08-09 17:29:08 -0300

    Add skeleton and links to coin details

--- src/page/Cryptocurrency.tsx
+++ src/page/Cryptocurrency.tsx
@@ -1,28 +1,38 @@
 import { useState, useEffect } from 'react'
-import HeaderRow from '../components/HeaderRow'
-import { MarketCoins }from '../types/Coins'
-import { getCoinMarket } from '../service/api'
-import CoinsRow from '../components/CoinsRow'
-import SearchBar from '../components/SearchBar'
-// import { Link } from 'wouter'
+import HeaderRow from '@/components/HeaderRow'
+import { MarketCoins }from '@/types/Coins'
+import { getCoinMarket } from '@/service/api'
+import CoinsRow from '@/components/CoinsRow'
+import SearchBar from '@/components/SearchBar'
+import CurrencySelect from '@/components/CurrencySelect'
+// import { Link } from 'wouter' 
+import { TableSkeleton } from '@/components/LoadingSkeleton'
 import {
   Table,
   TableBody,
-} from "@/components/ui/table"
+} from '@/components/ui/table'
 
 function Cryptocurrency() {
 
   const [coins, setCoins] = useState<MarketCoins[]>([])
   const [search, setSearch] = useState<string>('')
   const [contentPerPage, setContentPerPage] = useState(50)
+  const [currency, setCurrency] = useState<string>('usd')
+  const [loading, setLoading] = useState(true)  
+
+
+  const handleCurrencyChange = (value: string) => {
+    setCurrency(value)
+  }
 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await getCoinMarket(contentPerPage)
+        const res = await getCoinMarket({currency, contentPerPage})
         
         setCoins(res)
+        setLoading(false)
 
       } catch (error) {
         console.log(error)
@@ -30,7 +40,7 @@ function Cryptocurrency() {
 
     }
     fetchData()
-  },[contentPerPage])
+  },[currency,contentPerPage])
 
   const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLowerCase()) ||
@@ -43,26 +53,28 @@ function Cryptocurrency() {
 
   return (
     <>
-      <article className='w-full h-full pb-20 pt-16 text-white'>
+      <article className='w-full h-full pb-20 mt-16'>
         <h1 className=' ml-16 text-2xl'>Precios actuales</h1>
 
         <div className='flex justify-between mx-20 mt-14'>
-          <span className='mt-2 border-[1px] px-4 py-1 rounded-2xl border-lightGray text-grisLetras'>USD</span>
+          <CurrencySelect value={currency} onChange={handleCurrencyChange}/>
           <SearchBar search={search} setSearch={setSearch}/>
         </div>
 
         <div className='flex justify-center mt-11 mb-11 border-t-[1px] border-lightGray'>
           <Table> 
             <HeaderRow />
-            <TableBody>
-                {
-                  filteredCoins.slice(0, contentPerPage).map((coin, index) => (
-                    // <Link to={`/price/${coin.id}`} >
-                      <CoinsRow key={coin.id} coin={coin} index={index}/>
-                    // </Link>
-                  ))
-                }
-            </TableBody>
+            {
+              loading ? <TableSkeleton /> : (
+                <TableBody>
+                  {
+                    filteredCoins.slice(0, contentPerPage).map((coin, index) => (
+                      <CoinsRow key={coin.id}  coin={coin} index={index}/>
+                    ))
+                  }
+                </TableBody>
+                )
+            }
           </Table>
         </div>
 
@@ -71,7 +83,7 @@ function Cryptocurrency() {
             contentPerPage && (
               <button 
                 onClick={handleLoadContent}
-                className='w-44 p-1 rounded-2xl font-semibold border-2 border-lightGreen text-brightGreen bg-semiDarkGreen hover:bg-brightGreen hover:border-brightGreen hover:text-black transition duration-500'
+                className='w-44 p-1 rounded-xl font-semibold border-2 border-lightGreen text-brightGreen bg-semiDarkGreen hover:bg-brightGreen hover:border-brightGreen hover:text-black  transition duration-500'
               >Cargar Más</button>
             )
           }
