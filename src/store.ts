8f2e5043884907d7d575ea20ddcc5981f45e4f80
mordecaihicox <mordecaihicox@gmail.com>
2024-11-13 00:05:01 +0000

    implement store for managing search input state

--- src/store.ts
+++ src/store.ts
@@ -6,10 +6,19 @@ type Currency =  {
     setCurrency: (newCurrency: string) => void
 }
 
+type Search = {
+    search: string
+    setSearch: (value: string) => void
+}
 
 export const useCryptoStore = create<Currency>((set) => ({
     currency: 'usd',
     setCurrency: (newCurrency) => set({ currency: newCurrency }),
+}))
+
+export const useSearch = create<Search>((set) => ({
+    search: '',
+    setSearch: (value) => set({ search: value }),
 }))
 
 
