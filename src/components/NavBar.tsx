160658689378ec469cbc4443fabc87d82d575854
mordecaihicox <mordecaihicox@gmail.com>
2024-11-16 02:26:06 +0000

    Added SearchBar and CurrencySelect components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,27 @@
 import { Link } from 'wouter'
-import logo from '../assets/logo.png'
+import logo from '@/assets/logo.png'
+import SearchBar from './SearchBar'
+import CurrencySelect from './CurrencySelect'
 
 function NavBar() {
   return (
     <>
-      <nav className='w-full flex justify-between items-center px-10  h-24 bg-darkGreen border-b-[1px] border-lightGray'>
+      <nav className='fixed top-0 z-50 w-full flex justify-between items-center px-10  h-20 bg-darkGreen border-b-[1px] border-lightGray'>
         <Link to='/'>
           <div className='flex items-center'>
             <img 
               src={logo} 
               alt='Lynx logo' 
-              className='h-[4rem] w-[4.3rem] mr-1'
+              className='h-[2.8rem] w-[3rem] mr-1'
             />
-            <span className='font-sinkia text-logoText text-3xl font-bold select-none '>LYNX</span>
+            <span className='font-sinkia text-logoText text-2xl font-bold select-none '>LYNX</span>
           </div>
         </Link>
-        <Link to='/description'>
-          <button className='p-[6px] px-4 rounded-2xl font-semibold border-2 border-lightGreen text-brightGreen bg-semiDarkGreen hover:bg-brightGreen hover:border-brightGreen hover:text-black transition duration-500'>
-            Información
-          </button>
-        </Link>
+
+        <div className='flex items-center space-x-8 mr-[2rem]'> 
+          <SearchBar />
+          <CurrencySelect />
+        </div>
       </nav>
     </>
   )
