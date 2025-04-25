4c429b550216c4c0263873f0a59cb989e28ec8d7
Manuel <manuucarreras@gmail.com>
2025-04-24 21:21:10 -0300

    Added responsive design to components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from 'wouter'
 function Footer () {
   return(
     <>
-      <footer className='w-full h-[6rem] mt-[15rem] py-6 border-t-[1px] font-medium text-textGray border-lightGray flex items-center justify-center'>
+      <footer className='w-full h-[6rem] mt-[15rem] mini:mt-[6rem] py-6 border-t-[1px] font-medium text-textGray border-lightGray flex mini:flex-col items-center justify-center'>
         <p className=''>Lynx&copy; {new Date().getFullYear()} Created By Manuel Carreras - </p>
         <Link to='/description'>
           <button className='p-[6px] hover:underline'>
