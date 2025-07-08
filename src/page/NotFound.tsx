cefc9cf92d36bbfa920ef8cef482ad348504bb92
Manuel <manuucarreras@gmail.com>
2025-07-08 19:42:00 -0300

    Add Not Found page

---
+++ src/page/NotFound.tsx
@@ -0,0 +1,10 @@
+
+export function NotFound() {
+  return (
+    <section className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-8xl font-semibold text-brightGreen'>404</h1>
+      <p className='mt-4 text-xl text-gray-600'>Página no encontrada</p>
+      <p className='mt-2 text-base text-textGray'>Lo sentimos, la página que buscas no existe.</p>
+    </section>
+  )
+}
