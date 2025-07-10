d20c79f3353e01fdaf7fd3eeaa96e7bc16b3524b
Manuel <manuucarreras@gmail.com>
2025-07-10 20:16:09 -0300

    Add NotFound page to handle undefined routes

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -4,10 +4,11 @@ export function NotFound() {
 
   const [location] = useLocation()
 
+
   return (
     <>
       {
-        !(location == '/' || location == '/coin/:coinId' || location == '/description')  && (
+        !['/', '/description'].includes(location) && !location.startsWith('/coin/') && (
           <section className='flex flex-col items-center justify-center h-screen'>
             <h1 className='text-8xl font-semibold text-brightGreen'>404</h1>
             <p className='mt-4 text-xl text-gray-600'>Página no encontrada</p>
