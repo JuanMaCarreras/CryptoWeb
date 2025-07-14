972c211d42f3c135cd8ec7a1c7f635a2301410db
Manuel <manuucarreras@gmail.com>
2025-07-14 20:21:54 -0300

    Add Fav cell

--- src/components/CoinsRow.tsx
+++ src/components/CoinsRow.tsx
@@ -22,33 +22,34 @@ export function CoinsRow({coin, index}: CoinProp) {
   return (
     <>
       <TableRow className={`text-sm ${tableColor}`}>
-          <TableCell className='text-center' >
-             {coin.market_cap_rank}
-          </TableCell>
+        <TableCell className='text-center'>
+          <AddToFavorite coinId={coin.id} />
+        </TableCell>
+        
+        <TableCell className='text-center' >
+          {coin.market_cap_rank}
+        </TableCell>
 
-          <TableCell className='flex mini:ml-[-.8rem]'>
-            <img 
-              src={coin.image} 
-              alt={coin.name} 
-              className='mr-4 w-7 h-7 mt-1 mini:mr-3'
-            />
-            <div className='[&>span]:block'>
-              <span className='text-[1rem] font-medium'>{coin.name}</span>
-              <span className='text-xs uppercase text-textGray font-medium'>{coin.symbol}</span>
-            </div>
-          </TableCell>
+        <TableCell className='flex mini:ml-[-.8rem]'>
+          <img 
+            src={coin.image} 
+            alt={coin.name} 
+            className='mr-4 w-7 h-7 mt-1 mini:mr-3'
+          />
+          <div className='[&>span]:block'>
+            <span className='text-[1rem] font-medium'>{coin.name}</span>
+            <span className='text-xs uppercase text-textGray font-medium'>{coin.symbol}</span>
+          </div>
+        </TableCell>
 
-          <TableCell className='text-right'>${coin.current_price.toLocaleString()}</TableCell>
-          <TableCell className={` mini:hidden text-right ${priceColorClass}`}>{coin.price_change_percentage_24h.toFixed(2)}%</TableCell>
-          <TableCell className=' mini:hidden text-center'>${coin.market_cap.toLocaleString()}</TableCell>
-          <TableCell>
-            <Link to={`/coin/${coin.id}`} >
+        <TableCell className='text-right'>${coin.current_price.toLocaleString()}</TableCell>
+        <TableCell className={` mini:hidden text-right ${priceColorClass}`}>{coin.price_change_percentage_24h.toFixed(2)}%</TableCell>
+        <TableCell className=' mini:hidden text-center'>${coin.market_cap.toLocaleString()}</TableCell>
+        <TableCell>
+          <Link to={`/coin/${coin.id}`} >
               <HiOutlineExternalLink className='h-5 w-5 text-textGray hover:text-brightGreen transition duration-500'/>
-            </Link>
-          </TableCell>
-          <TableCell className='text-center'>
-            <AddToFavorite coinId={coin.id} />
-          </TableCell>
+          </Link>
+        </TableCell>
       </TableRow>  
     </>
   )
