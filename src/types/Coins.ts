4366e891834859ee5cb22191b13b2d8a19470ac4
Manuel <manuucarreras@gmail.com>
2025-07-18 20:05:50 -0300

    Add favorite interface

--- src/types/Coins.ts
+++ src/types/Coins.ts
@@ -1,50 +1,57 @@
 export interface MarketCoins {
-    id: string
-    symbol: string
-    name: string
-    image: string
-    current_price: number
-    market_cap: number
-    market_cap_rank: number
-    total_volume: number
-    price_change_percentage_24h: number
-    market_cap_change_percentage_24h: number
+  id: string
+  symbol: string
+  name: string
+  image: string
+  current_price: number
+  market_cap: number
+  market_cap_rank: number
+  total_volume: number
+  price_change_percentage_24h: number
+  market_cap_change_percentage_24h: number
 }
 
 interface Description {
-    es: string
-    [key: string]: string
+  es: string
+  [key: string]: string
 }
 
 interface ImgaeValue {
-    [key:string]: string
+  [key:string]: string
 }
 
 interface MarketValue {
-    [key: string]: number
+  [key: string]: number
 }
 
 interface  MarketData{
-    current_price: MarketValue
-    market_cap: MarketValue
-    price_change_24h: number
-    high_24h: { [key: string]: number }
-    low_24h: { [key: string]: number }
+  current_price: MarketValue
+  market_cap: MarketValue
+  price_change_24h: number
+  high_24h: { [key: string]: number }
+  low_24h: { [key: string]: number }
 }
 
 
 export interface CoinDetailsById {
-    id: string
-    symbol: string
-    name: string
-    description: Description
-    image: ImgaeValue
-    market_data: MarketData
+  id: string
+  symbol: string
+  name: string
+  description: Description
+  image: ImgaeValue
+  market_data: MarketData
 }
 
 export interface CoinMarketChart {
-    prices : [number, number] []
-    market_caps: [number, number][]
-    total_volumes: [number, number][]
+  prices : [number, number] []
+  market_caps: [number, number][]
+  total_volumes: [number, number][]
 }
 
+
+export interface FavCoins extends Pick<MarketData, 'current_price'> {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+}
