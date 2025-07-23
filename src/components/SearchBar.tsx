11f063eabfaa0471129372d0d698d62e02c9d637
Manuel <manuucarreras@gmail.com>
2025-07-23 19:52:40 -0300

    change border-color

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -19,7 +19,7 @@ export function SearchBar() {
           placeholder='Buscar...'
           value={search}
           onChange={(e) => handleChange(e.target.value)}
-          className='outline-none w-[20rem] h-[2rem] pl-4 py-4 bg-deepGreen '
+          className='outline-none w-[20rem] h-[2rem] pl-4 py-4 bg-deepGreen border-lightGray'
         />
       </div>
     </>
