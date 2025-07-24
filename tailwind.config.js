9330b1c922a629ccda59fcb708d36a7a540aafb3
Manuel <manuucarreras@gmail.com>
2025-07-24 19:19:01 -0300

    Add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
-    screens:{
-      mini: {min :'330px', max: '440px'},
+    screens: {
+      mini: { min: '330px', max: '440px' },
     },
     fontFamily: {
-      popins: ['Poppins', 'sans-serif',],
-      sinkia: ['Signika Negative', 'sans-serif']
+      popins: ['Poppins', 'sans-serif'],
+      sinkia: ['Signika Negative', 'sans-serif'],
     },
     colors: {
       black: '#000000',
@@ -30,34 +30,34 @@ module.exports = {
       darkGray: '#0F1110',
       deepGray: '#0B0C0D',
       textGray: '#868C8A',
-      negativeNum: '#F87171',
+      negativeNum: '#ff4040',
       positiveNum: '#4ADE80',
       skeletonGray: '#18181b',
-      skeletonGray2: '#27272a'
+      skeletonGray2: '#27272a',
     },
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 }
