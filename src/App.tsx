06fc132921d42bf395f8f707c06d3aa12324998c
Manuel <manuucarreras@gmail.com>
2025-07-28 20:12:10 -0300

    Add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { ToTop } from '@/components/ToTop'
 import { NavBar } from '@/components/NavBar'
-import { Cryptocurrency } from '@/page/Cryptocurrency'
+import { Home } from '@/page/Home'
 import { Description } from '@/page/Description'
 import { Details } from '@/page/Details'
 import { Footer } from '@/components/Footer'
 import { NotFound } from '@/page/NotFound'
 import { Route, Switch } from 'wouter'
 import './App.css'
-
 
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
       <NavBar />
       <Switch>
         <main className='mx-11 mini:mx-5 h-full flex-1'>
-          <Route path='/' component={Cryptocurrency}  />
+          <Route path='/' component={Home}  />
           <Route path='/description' component={Description} />
           <Route path='/coin/:coinId' component={Details} />
           <Route component={NotFound} />
