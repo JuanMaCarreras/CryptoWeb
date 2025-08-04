8f41e1db3f88f72ba569f5aaf305e6fa28d93bee
Manuel <manuucarreras@gmail.com>
2025-08-04 19:56:36 -0300

    main (app)

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
-      afterSignOutUrl='/'
+      afterSignOutUrl='/' 
     >
       <App />
     </ClerkProvider>
