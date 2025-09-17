2c17857a14c63f8ec72ee4bee8e8ef237fd4899e
Manuel <manuucarreras@gmail.com>
2025-09-17 20:24:35 -0300

    profile component

--- src/components/auth/Profile.tsx
+++ src/components/auth/Profile.tsx
@@ -1,5 +1,9 @@
+import { useAuthStore } from '@/store'
 
 export function Profile() {
+
+  const { user } = useAuthStore()
+  console.log('user profile', user)
   return (
     <div>Profile</div>
   )
