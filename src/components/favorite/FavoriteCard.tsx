cbbd5c1fe125f65f08b77775b82c7486c563bb1e
Manuel <manuucarreras@gmail.com>
2025-09-18 20:28:43 -0300

    Fix carousel component layout and functionality

--- src/components/favorite/FavoriteCard.tsx
+++ src/components/favorite/FavoriteCard.tsx
@@ -3,7 +3,6 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle,
 } from '@/components/ui/card'
 import { CoinDetails } from '@/types/Coins'
 import { useCryptoStore } from '@/store'
@@ -19,30 +18,36 @@ export function FavoriteCard({ coin }: FavoriteCardProps ) {
   
   return (
     <>
-      <Card 
+      <Card
         key={coin.id}
-        className='w-full max-w-md flex flex-row items-center justify-between gap-10 p-2 border-lightGray bg-darkGreen  hover:bg-hoverGreen transition-colors duration-700'>
-          
-        <CardHeader>
-          <CardTitle className='flex flex-col items-end gap-2'>
-            <div className='flex items-center gap-2'>
-            <img 
-              src={coin.image.small} 
-              alt={coin.name} 
-              className='w-6 h-6'
-            />
-            <p className='text-lg uppercase  text-textGray font-medium'>{coin.symbol}</p>
-          </div>
-          </CardTitle>
-          <CardDescription>
-            <p className='text-sm'>{coin.name}</p>
+        className="w-64 h-28 border-lightGray rounded-lg bg-darkGreen hover:bg-hoverGreen transition-colors duration-500"
+      >
+        <CardHeader className="p-3 flex flex-row items-center gap-2">
+          <img
+            src={coin.image.small}
+            alt={coin.name}
+            className="w-7 h-7"
+          />
+          <CardDescription className="text-lg text-gray-400 font-medium pb-1">
+            {coin.name}
+          </CardDescription>
+          <CardDescription className='text-base font-semibold text-textGray pb-1'>
+            {coin.symbol}
           </CardDescription>
         </CardHeader>
-        <CardContent className='flex flex-col items-items-start'>
-          <p className={`text-sm font-semibold ${priceColorClass}`}>{coin.market_data.price_change_percentage_24h.toFixed(2)}% (24H)</p>
-          <p className='text-lg'>${coin.market_data.current_price[currency]}</p>
+
+        <CardContent className="pb-42flex flex-col gap-1">
+          <div className="flex items-center justify-between">
+            <p className="text-base font-bold text-white">
+              ${coin.market_data.current_price[currency]}
+            </p>
+            <p className={`text-sm font-semibold ${priceColorClass}`}>
+              {coin.market_data.price_change_percentage_24h.toFixed(2)}% (24H)
+            </p>
+          </div>
         </CardContent>
-      </Card> 
+      </Card>
+
     </>
   )
 }
