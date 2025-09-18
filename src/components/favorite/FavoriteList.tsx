06a2c8451d05f573d8e3a8037f3a8696f9b7e05a
Manuel <manuucarreras@gmail.com>
2025-09-18 20:28:20 -0300

    Fix carousel component layout and functionality

--- src/components/favorite/FavoriteList.tsx
+++ src/components/favorite/FavoriteList.tsx
@@ -24,7 +24,7 @@ export function FavoriteList() {
   useEffect(() => {
     const fetchFavorites = async () => {
       if (user) {
-        const favs = await getUserFavorites(user.uid)
+        const favs = await getUserFavorites(user?.uid)
         
         const details = await Promise.all(
           favs.map((coinId: string) => getCoinById(coinId))
@@ -40,25 +40,28 @@ export function FavoriteList() {
 
   return (
     <>
-      <h2 className='text-xl ml-10'>Favoritos</h2>
-      <section className='flex flex-row justify-center gap-4 mb-20 mt-4'>
-        <Carousel>
-          <CarouselContent>
-            {
-              coins.map((coin, index) => (
-                <CarouselItem key={index} className='basis-1/3'>
-                  <Link to={`/coin/${coin.id}`} >
-                    <FavoriteCard coin={coin} />
-                  </Link>
-                </CarouselItem>
-              ))
-            }
-          </CarouselContent>
+      <section>
+        <h2 className='text-xl ml-10'>Favoritos</h2>
 
-          <CarouselPrevious />
-          <CarouselNext />
-        
-        </Carousel>
+        <div className='flex flex-row justify-center gap-2 mb-20 mt-4'>
+          <Carousel className='w-full max-w-6xl' >
+            <CarouselContent>
+              {
+                coins.map((coin, index) => (
+                  <CarouselItem key={index} className='basis-1/4' >
+                    <Link to={`/coin/${coin.id}`} >
+                      <FavoriteCard coin={coin} />
+                    </Link>
+                  </CarouselItem>
+                ))
+              }
+            </CarouselContent>
+
+            <CarouselPrevious className='hover:bg-bg-deepGreen border-[.1rem] border-lightGray rounded-full hover:bg-brightGreen hover:text-black hover:border-[.1rem] hover:border-logoText  transition duration-700'/>
+            <CarouselNext className='hover:bg-bg-deepGreen border-[.1rem] border-lightGray rounded-full hover:bg-brightGreen hover:text-black hover:border-[.1rem] hover:border-logoText  transition duration-700'/>
+          
+          </Carousel>
+        </div>
       </section>
     </>
   )
