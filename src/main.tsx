4162bf931a0da66aec6e45ee2b00375d8f522818
Manuel <manuucarreras@gmail.com>
2025-09-24 19:58:36 -0300

    Replaced Clerk auth buttons with custom Firebase auth components

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      afterSignOutUrl='/' 
-    >
-      <App />
-    </ClerkProvider>
+    <App />
   </React.StrictMode>,
 )
