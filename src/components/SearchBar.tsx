0f706baccd012d7247cdef7a68b4a9c50d915024
Manuel <manuucarreras@gmail.com>
2025-10-02 20:20:08 -0300

    Enhance searchbar for improved user experience

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { Input } from '@/components/ui/input'
 import { useSearch } from '@/store'
-
+import { LuX } from 'react-icons/lu'
 
 export function SearchBar() {
   
@@ -11,18 +11,32 @@ export function SearchBar() {
     setSearch(value)
   }
 
+  const handleClear = () => {
+    setSearch('')
+  }
+
   return (
     <>
-      <div className=''>
+      <div className='relative'>
         <Input 
           type='text' 
           placeholder='Buscar...'
           value={search}
           onChange={(e) => handleChange(e.target.value)}
-          className='outline-none w-[20rem] h-[2rem] pl-4 py-4 bg-deepGreen border-lightGray'
+          className='outline-none w-[20rem] h-[2rem] pl-4 py-4 pr-10 bg-deepGreen border-lightGray'
         />
+        
+        {
+          search && (
+            <button
+              onClick={handleClear}
+              className='absolute right-3 top-1/2 -translate-y-1/2 p-1 hover:bg-gray-700 hover:bg-opacity-50 rounded-full transition-colors'
+              aria-label='Borrar búsqueda'
+            >
+              <LuX className='w-4 h-4 text-textGray'/>
+            </button>
+        )}
       </div>
     </>
   )
 }
-
