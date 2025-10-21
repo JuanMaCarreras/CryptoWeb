462e21e1f748796c0de8fa4d92335021c6be36eb
Manuel <manuucarreras@gmail.com>
2025-10-21 19:42:33 -0300

    Add Not Found component for invalid URLs

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -1,21 +1,14 @@
-import { useLocation } from 'wouter'
 
 export function NotFound() {
-
-  const [location] = useLocation()
 
 
   return (
     <>
-      {
-        !['/', '/description'].includes(location) && !location.startsWith('/coin/') && (
-          <section className='flex flex-col items-center justify-center h-screen'>
-            <h1 className='text-8xl font-semibold text-brightGreen'>404</h1>
-            <p className='mt-4 text-xl text-gray-600'>Página no encontrada</p>
-            <p className='mt-2 text-base text-textGray'>Lo sentimos, la página que buscas no existe.</p>
-          </section>
-        )
-      }
+      <section className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='text-8xl font-semibold text-brightGreen'>404</h1>
+        <p className='mt-4 text-xl text-gray-600'>Página no encontrada</p>
+        <p className='mt-2 text-base text-textGray'>Lo sentimos, la página que buscas no existe.</p>
+      </section>
     </>
   )
 }
