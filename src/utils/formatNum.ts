084b111a5969eafed61b738410258bcee348f810
Manuel <manuucarreras@gmail.com>
2026-02-05 19:32:26 -0300

    Format Market Cap. function

--- src/utils/formatNum.ts
+++ src/utils/formatNum.ts
@@ -1,6 +1,10 @@
 
 
-export function formatMarketCap(num: number): string {
+export function formatMarketCap(num: number ): string {
+  if (num === null || num === undefined) {
+    return '$0'
+  }
+  
   const units = [
     { value: 1_000_000_000_000, symbol: 'T' },
     { value: 1_000_000_000, symbol: 'B' },
@@ -13,7 +17,7 @@ export function formatMarketCap(num: number): string {
       return `$${(num / value).toFixed(2)}${symbol}`
     }
   }
-  const strNum = num.toString
+  const strNum = num.toString()
   return `$${strNum}`
 }
 
