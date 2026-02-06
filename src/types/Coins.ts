31dd2967cb000801e456faac95dac53166657beb
Manuel <manuucarreras@gmail.com>
2026-02-06 20:24:00 -0300

    refactor(types): standardize MarketData typing and clean up interfaces

--- src/types/Coins.ts
+++ src/types/Coins.ts
@@ -1,3 +1,75 @@
+// export interface MarketCoins {
+//   id: string
+//   symbol: string
+//   name: string
+//   image: string
+//   current_price: number
+//   market_cap: number
+//   market_cap_rank: number
+//   total_volume: number
+//   price_change_percentage_24h: number
+//   market_cap_change_percentage_24h: number
+// }
+
+// interface Description {
+//   es: string
+//   [key: string]: string
+// }
+
+// interface ImgaeValue {
+//   [key:string]: string
+// }
+
+// interface MarketValue {
+//   [key: string]: number
+// }
+
+// interface  MarketData{
+//   current_price: MarketValue
+//   market_cap: MarketValue
+//   price_change_percentage_24h: number
+//   price_change_24h: number
+//   total_volume: Record<string, number>
+//   high_24h: { [key: string]: number }
+//   low_24h: { [key: string]: number }
+// }
+
+
+// export interface CoinDetails {
+//   id: string
+//   market_cap_rank: number
+//   symbol: string
+//   name: string
+//   description: Description
+//   image: ImgaeValue
+//   market_data: MarketData
+// }
+
+// export interface CoinDetailProps {
+//   market_cap_rank: number
+//   market_data: {
+//     market_cap: Record<string, number>
+//     total_volume: Record<string, number>
+//     high_24h: Record<string, number>
+//     low_24h: Record<string, number>
+//   }
+// }
+
+// export interface CoinMarketChart {
+//   prices : [number, number] []
+//   market_caps: [number, number][]
+//   total_volumes: [number, number][]
+// }
+
+
+// export interface ChartPoint {
+//   date: number   // timestamp (ms)
+//   price: number
+// }
+
+
+export type CurrencyMap = Record<string, number>
+
 export interface MarketCoins {
   id: string
   symbol: string
@@ -12,56 +84,42 @@ export interface MarketCoins {
 }
 
 interface Description {
-  es: string
   [key: string]: string
 }
 
-interface ImgaeValue {
-  [key:string]: string
+interface ImageValue {
+  thumb: string
+  small: string
+  large: string
 }
 
-interface MarketValue {
-  [key: string]: number
-}
-
-interface  MarketData{
-  current_price: MarketValue
-  market_cap: MarketValue
+export interface MarketData {
+  current_price: CurrencyMap
+  market_cap: CurrencyMap
+  total_volume: CurrencyMap
+  high_24h: CurrencyMap
+  low_24h: CurrencyMap
   price_change_percentage_24h: number
   price_change_24h: number
-  total_volume: number
-  high_24h: { [key: string]: number }
-  low_24h: { [key: string]: number }
 }
-
 
 export interface CoinDetails {
   id: string
+  market_cap_rank: number
   symbol: string
   name: string
   description: Description
-  image: ImgaeValue
+  image: ImageValue
   market_data: MarketData
 }
 
-export interface CoinDetailProps {
-  market_cap_rank: number;
-  market_data: {
-    market_cap: Record<string, number>
-    total_volume: Record<string, number>
-    high_24h: Record<string, number>
-    low_24h: Record<string, number>
-  }
-}
-
 export interface CoinMarketChart {
-  prices : [number, number] []
+  prices: [number, number][]
   market_caps: [number, number][]
   total_volumes: [number, number][]
 }
 
-
 export interface ChartPoint {
-  date: number   // timestamp (ms)
+  date: number
   price: number
 }
