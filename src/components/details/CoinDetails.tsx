cce9717d050f799db4752ce12b3bd72ec67ff349
Manuel <manuucarreras@gmail.com>
2026-03-04 19:27:03 -0300

    refactor: move API logic to reusable custom hook

--- src/components/details/CoinDetails.tsx
+++ src/components/details/CoinDetails.tsx
@@ -6,6 +6,8 @@ import { MarketData } from './MarketData'
 import { Card } from '../ui/card'
 import { CoinDetailsHeader } from './CoinDetailsHeader'
 import { useCoinDetails } from '@/hook/useCoinDetails'
+import { Spinner } from '../ui/spinner'
+
 
 export function CoinDetails() {
 
@@ -13,15 +15,15 @@ export function CoinDetails() {
   const { coinId } = useParams<{coinId: string}>()
   const { coins, loading } = useCoinDetails(coinId)
 
+  if(loading) return <Spinner className='size-20' />
+
   return (
     <section className='w-full flex justify-center my-5'>
       <Card className='w-full pb-9 overflow-hidden'>
         <div className='flex flex-col [@media(min-width:820px)]:flex-row'>
-          
-          {/* Header + Chart */}
           <div className='flex flex-col gap-11 mb-6 [@media(min-width:820px)]:mb-14 w-full [@media(min-width:820px)]:flex-1 min-w-0'>
             <CoinDetailsHeader loading={loading} coins={coins} coinId={coinId}/>
-            <div className='w-full h-[25rem]'>
+            <div className='w-full h-[25rem] pl-10'>
               <Chart coinId={coinId} currency={currency} />
             </div>
           </div>
