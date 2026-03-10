b7272b68ba7ebd5c207a58cae9346f72022b8e91
Manuel <manuucarreras@gmail.com>
2026-03-10 20:10:59 -0300

    feat: add lazy loading to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { ToTop } from '@/components/ToTop'
-import { NavBar } from '@/components/NavBar'
-import { Home } from '@/page/Home'
-import { Description } from '@/page/Description'
-import { Details } from '@/page/Details'
-import { Footer } from '@/components/Footer'
-import { NotFound } from '@/page/NotFound'
-import { Route, Switch } from 'wouter'
-import './App.css'
-
+import { lazy } from 'react'
 import { Register } from '@/page/Register'
 import { Login } from '@/page/Login'
+import { NotFound } from '@/page/NotFound'
+import { ToTop } from '@/components/ToTop'
+import { NavBar } from '@/components/NavBar'
+import { Footer } from '@/components/Footer'
+import { Route, Switch } from 'wouter'
+import './App.css'
 import { useAuthListener } from '@/hook/useAuthListener'
 import { PasswordReset } from '@/page/PasswordReset'
 
+
+const Home = lazy(() => import('@/page/Home'))
+const Description = lazy(() => import('@/page/Description'))
+const Details = lazy(() => import('@/page/Details'))
 
 function App() {
   useAuthListener()
