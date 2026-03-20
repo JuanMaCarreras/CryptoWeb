ae6aebf27b56e9e83dc623dd0419507c901e67b5
Manuel <manuucarreras@gmail.com>
2026-03-20 19:37:46 -0300

    fix: add terser dependency for production build

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,7 @@ export default defineConfig({
     },
   },
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      }
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
