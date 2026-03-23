0b4e1daabd4b1f851fed32418df27c6afa8d9505
Manuel <manuucarreras@gmail.com>
2026-03-23 18:44:22 -0300

    integrate Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+      <Analytics />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
