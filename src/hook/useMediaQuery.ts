2925f89f38d89880547d3fe5634533023ba6b7a7
Manuel <manuucarreras@gmail.com>
2026-04-08 19:06:27 -0300

    add sheet ui component

---
+++ src/hook/useMediaQuery.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+
+    const listener = () => setMatches(media.matches);
+    media.addEventListener("change", listener);
+
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
+}
